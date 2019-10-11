f30d37176970e3b547ebac3cb6c1d7d145732651
PabloRRBASF <pablo.ruiz@basf.com>
2019-10-11 14:19:17 +0200

    repre

--- SNE.py
+++ SNE.py
@@ -7,12 +7,10 @@ colors = ['red', 'blue', 'green']
 
 # High Dimensional Space
 
-p1 = np.array([1,6,0]).T
-p2 = np.array([3,5,1]).T
-p3 = np.array([7,1,5]).T
+p1 = np.array([1,6,6]).T # red
+p2 = np.array([3,5,1]).T # blue
+p3 = np.array([7,1,5]).T # green
 
-
-# def plot_3D(matrix):
 
 def adjusts_axis(points):
     return np.array()
@@ -20,17 +18,14 @@ def adjusts_axis(points):
 
 points = np.array([p1,p2,p3])
 
+def plot_3D(matrix):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(*matrix, c=colors, s=100)
+    ax.invert_yaxis()
+    plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(*points, c=colors, s=100)
-ax.invert_yaxis()
-plt.show()
-
-
-for (i,j,k) in zip(*points.T):
-    print(i,j,k)
-
+plot_3D(points)
 
 sigmas = np.array([0.2,0,3,0.5])
 
